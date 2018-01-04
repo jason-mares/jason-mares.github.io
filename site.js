9d6612f14d216095059005a6d8b43fce0927a1fc
Jason <jasonthomasdesign@gmail.com>
2018-01-03 23:47:53 -0800

    reactive header

--- site.js
+++ site.js
@@ -29,26 +29,86 @@ m(e)});if(d)return b(c,d-1)},f=document.querySelectorAll("a.scroll"),g=f.length-
 ///////////////////////////
 
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+//window.onscroll = function() {myFunction()};
+//
+//// Get the header
+//var header = document.getElementById("site-nav");
+//var wrapper = document.getElementById("wrapper");
+//
+//// Get the offset position of the navbar
+//var sticky = header.offsetTop;
+//
+//// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+//function myFunction() {
+//  if (window.pageYOffset >= sticky) {
+//    header.classList.add("sticky");
+//    wrapper.classList.add("wrapper-padding");
+//  } else {
+//    header.classList.remove("sticky");
+//    wrapper.classList.remove("wrapper-padding");
+//  }
+//}
 
-// Get the header
-var header = document.getElementById("site-nav");
-var wrapper = document.getElementById("wrapper");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-    wrapper.classList.add("wrapper-padding");
-  } else {
-    header.classList.remove("sticky");
-    wrapper.classList.remove("wrapper-padding");
-  }
-}
 
 
 
+
+
+
+
+///////////////////////////
+/////// STICKY FIXED FOOTER
+///////////////////////////
+//...
+/*
+		By Osvaldas Valutis, www.osvaldas.info
+		Available for use under the MIT License
+	*/
+
+	
+	;( function ( document, window, index )
+	{
+		'use strict';
+
+		var elSelector	= '.header',
+			element		= document.querySelector( elSelector );
+
+		if( !element ) return true;
+
+		var elHeight		= 0,
+			elTop			= 0,
+			dHeight			= 0,
+			wHeight			= 0,
+			wScrollCurrent	= 0,
+			wScrollBefore	= 0,
+			wScrollDiff		= 0;
+
+		window.addEventListener( 'scroll', function()
+		{
+			elHeight		= element.offsetHeight;
+			dHeight			= document.body.offsetHeight;
+			wHeight			= window.innerHeight;
+			wScrollCurrent	= window.pageYOffset;
+			wScrollDiff		= wScrollBefore - wScrollCurrent;
+			elTop			= parseInt( window.getComputedStyle( element ).getPropertyValue( 'bottom' ) ) + wScrollDiff;
+
+			if( wScrollCurrent <= 0 ) // scrolled to the very top; element sticks to the top
+				element.style.bottom = '0px';
+
+			else if( wScrollDiff > 0 ) // scrolled up; element slides in
+				element.style.bottom = ( elTop > 0 ? 0 : elTop ) + 'px';
+
+			else if( wScrollDiff < 0 ) // scrolled down
+			{
+				if( wScrollCurrent + wHeight >= dHeight - elHeight )  // scrolled to the very bottom; element slides in
+					element.style.bottom = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
+
+				else // scrolled down; element slides out
+					element.style.bottom = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px';
+			}
+
+			wScrollBefore = wScrollCurrent;
+		});
+
+	}( document, window, 0 ));
 
